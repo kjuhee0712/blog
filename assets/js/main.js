24304d41295d289d34afe8f22f87cff29e1014d4
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-01 17:10:47 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,27 +49,10 @@ $(document).ready(function(){
 	//브라우저 사이즈에 따라 작동
 	var bw=$(window).width();
 
-	if(bw > 1024){
-		
+	if(bw > 1024){	
 			
 	}else{
-		var bi = $('.main_banner .slick-slide a img');
-		console.log(bi)
-		
 
-		$.each(bi, function (index, item) { 
-
-			console.log(item);
-			var bis = item.getAttribute('src').split('_');
-			console.log(bis);
-			bis = bis.split('_');
-	        console.log(bis);
-			item.attr('src',bis[0]+'_'+bis[1]+'.jpg');
-
-		// 두 번째 매개변수로는 콜백함수인데 콜백함수의 매개변수 중 
-		// 첫 번째 index는 배열의 인덱스 또는 객체의 키를 의미하고 
-		// 두 번째 매개 변수 item은 해당 인덱스나 키가 가진 값을 의미합니다. var result = ''; result += index +' : ' + item.title + ', ' + item.url; console.log(result); // 0 : 다음, http://daum.net // 1 : 네이버, http://naver.com })
-		});
 	}
 
 });
