623d7a9e65d1e85947bb61f8cc6c0c4c8d12b8a4
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-01 13:28:03 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,25 +9,44 @@ $(document).ready(function(){
 
 	//브라우저 사이즈에 따라 배너 작동
 	var bw=$(window).width();
-	if(bw > 1024){
 
-		$('.main_banner').slick({
+	$('.main_banner').slick({
 		    infinite: true,
 	        centerMode: true,
 	        centerPadding: '120px',
 	        slidesToShow: 3,
-	        slidesToScroll: 3
-		});		
+	        slidesToScroll: 3,
+	        responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2,
+		        infinite: true,
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		});	
+
+
+	if(bw > 1024){
+
+			
 
 	}else{
 
-		$('.main_banner').slick({
-		    infinite: true,
-	        centerMode: true,
-	        centerPadding: '120px',
-	        slidesToShow: 2,
-	        slidesToScroll: 1
-		});	
 
 	}
 
@@ -36,29 +55,6 @@ $(document).ready(function(){
 
 $(window).on('resize', function(){
 
-	//브라우저 사이즈에 따라 배너 작동
-	var bw=$(window).width();
-	if(bw > 1024){
-
-		$('.main_banner').slick({
-		    infinite: true,
-	        centerMode: true,
-	        centerPadding: '120px',
-	        slidesToShow: 3,
-	        slidesToScroll: 3
-		});		
-
-	}else{
-
-		$('.main_banner').slick({
-		    infinite: true,
-	        centerMode: true,
-	        centerPadding: '120px',
-	        slidesToShow: 2,
-	        slidesToScroll: 1
-		});	
-
-	}
 	
 });
 	
