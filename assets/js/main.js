dd710cea232bf807402f3bb50aa33acc0e235288
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-01 16:53:31 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,11 +59,12 @@ $(document).ready(function(){
 
 		$.each(bi, function (index, item) { 
 
-			var bis = index.attr('src');
+			console.log(item);
+			var bis = item.attr('src');
 			console.log(bis);
 			bis = bis.split('_');
 	        console.log(bis);
-			index.attr('src',bis[0]+'_'+bis[1]+'.jpg');	
+			item.attr('src',bis[0]+'_'+bis[1]+'.jpg');	
 
 		// 두 번째 매개변수로는 콜백함수인데 콜백함수의 매개변수 중 
 		// 첫 번째 index는 배열의 인덱스 또는 객체의 키를 의미하고 
