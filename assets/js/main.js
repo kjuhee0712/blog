51c19fde9ba7da400e61094df777cc5438274326
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-01 16:50:10 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,7 @@ $(document).ready(function(){
 		// 두 번째 매개변수로는 콜백함수인데 콜백함수의 매개변수 중 
 		// 첫 번째 index는 배열의 인덱스 또는 객체의 키를 의미하고 
 		// 두 번째 매개 변수 item은 해당 인덱스나 키가 가진 값을 의미합니다. var result = ''; result += index +' : ' + item.title + ', ' + item.url; console.log(result); // 0 : 다음, http://daum.net // 1 : 네이버, http://naver.com })
-		}
+		});
 
 });
 	
