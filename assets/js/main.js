e2ec484a5a700ce3922e1b55354c4e59f0dd6b50
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-05 17:53:37 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,27 @@ $(document).ready(function(){
 
 	}
 
+	var $footer_btn = $('a.btn_top, a.btn_home, a.btn_menu');
+	
+	$('a.btn_top').hide(); // 탑 버튼 숨김
+
+			 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) { // 스크롤 내릴 표시
+			$('a.btn_top').fadeIn();
+		} else {
+			$('a.btn_top').fadeOut();
+		}
+	});
+
+
+	$('a.btn_top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);  // 탑 이동 스크롤 속도
+		return false;
+	});
+
 });
 	
 
