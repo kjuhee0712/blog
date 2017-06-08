79c457cbd2a2275e5a4df6fd99d57b4e22e25e0e
DESKTOP-3MOISS6\tri-0070 <kjuhee0712@gmail.com>
2017-06-08 16:56:22 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,7 +76,7 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$('a.btn_home').click(function () {
+	$('a.btn_menu').click(function () {
 		$('.header_bg').animate({ 'width': 'toggle' }, 500);
 		console.log($(this));
 	});	
